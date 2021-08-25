278f0c7464860dc549b9bb1b6bde74e3a57c92e6
Tsekov David <tsekov.david@yandex.ru>
2021-08-25 19:04:10 +0300

    My first commit

--- homework.py
+++ homework.py
@@ -1,9 +1,82 @@
 import datetime as dt
-
-
-class Calculator:
-    pass
+date_format = '%d.%m.%Y'
 
 
 class Record:
-    pass
+ 
+    def __init__(self, amount, comment, date = None):
+        self.amount = amount
+        self.comment = comment
+
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, date_format).date()
+
+
+class Calculator:
+
+    def __init__(self, limit: int) -> None:
+        self.limit = limit
+        self.records: float = []
+        #подумать о типе данных в records
+
+    def add_record(self, record: Record):
+        self.records.append(record)
+        """Создает список из поступающих объктов класса Record 
+        и записывает в список в records.
+        """
+
+    def get_week_stats(self):
+        today = dt.date.today()
+        last_week = today - dt.timedelta(days=7)
+        total_stats = 0
+        for i in self.records:
+            if last_week <= i.date <= today:
+                total_stats += i.amount
+        return total_stats
+
+    def get_today_stats(self):
+        today = dt.date.today()
+        total_stats = 0
+        for i in self.records:
+            if i.date == today:
+                total_stats += i.amount
+        return total_stats
+
+    def remainder(self):
+        balance = self.limit - self.get_today_stats()
+        return balance
+
+
+class CashCalculator(Calculator):
+ 
+    USD_RATE: float = 74.00
+    EURO_RATE: float = 86.00
+    RUB_RATE: float = 1.00
+
+    def get_today_cash_remained(self, currency = 'rub'):
+        exchange_rate = {
+            'usd': (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro'),
+            'rub': (self.RUB_RATE, 'руб')
+            }
+        cost, coin = exchange_rate[currency]
+        difference = round(self.remainder() / cost, 2)
+        
+        if self.limit >= difference:
+            return f'На сегодня осталось {difference} {coin}'
+        elif difference == 0:
+            return 'Денег нет, держись'
+        else:
+            return f'Денег нет, держись: твой долг - {difference} {coin}'
+
+
+class CaloriesCalculator(Calculator):
+    
+    def get_calories_remained(self): 
+        balance = self.remainder()
+        if balance > 0:
+            return f'Сегодня можно съесть что-нибудь ещё, '
+            f'но с общей калорийностью не более {balance} кКал'
+        return 'Хватит есть!'
