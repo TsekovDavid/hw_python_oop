07f95bed01acba4682bdbe759e6525cf1d52be70
Tsekov David <tsekov.david@yandex.ru>
2021-08-25 21:49:05 +0300

    complete tests

--- homework.py
+++ homework.py
@@ -19,7 +19,6 @@ class Calculator:
     def __init__(self, limit: int) -> None:
         self.limit = limit
         self.records: float = []
-        #подумать о типе данных в records
 
     def add_record(self, record: Record):
         self.records.append(record)
@@ -45,30 +44,32 @@ class Calculator:
         return total_stats
 
     def remainder(self):
-        balance = self.limit - self.get_today_stats()
+        balance: float = self.limit - self.get_today_stats()
         return balance
 
 
 class CashCalculator(Calculator):
- 
+
     USD_RATE: float = 74.00
     EURO_RATE: float = 86.00
     RUB_RATE: float = 1.00
 
-    def get_today_cash_remained(self, currency = 'rub'):
+    def get_today_cash_remained(self, currency):
+        remain = self.remainder()#лимит минус количество трат за сегодня
+        if remain == 0:
+            return 'Денег нет, держись'
         exchange_rate = {
             'usd': (self.USD_RATE, 'USD'),
             'eur': (self.EURO_RATE, 'Euro'),
             'rub': (self.RUB_RATE, 'руб')
             }
         cost, coin = exchange_rate[currency]
-        difference = round(self.remainder() / cost, 2)
-        
-        if self.limit >= difference:
+        difference = round(remain / cost, 2)#limit минус количество за сегодня, деленное на курс
+
+        if remain > 0:
             return f'На сегодня осталось {difference} {coin}'
-        elif difference == 0:
-            return 'Денег нет, держись'
         else:
+            difference = abs(difference)
             return f'Денег нет, держись: твой долг - {difference} {coin}'
 
 
@@ -77,6 +78,22 @@ class CaloriesCalculator(Calculator):
     def get_calories_remained(self): 
         balance = self.remainder()
         if balance > 0:
-            return f'Сегодня можно съесть что-нибудь ещё, '
-            f'но с общей калорийностью не более {balance} кКал'
-        return 'Хватит есть!'
+            return (
+                'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью '
+                f'не более {balance} кКал'
+            )
+        else:
+            return 'Хватит есть!'
+
+r1 = Record(amount=145, comment='Безудержный шопинг', date='08.03.2019')
+r2 = Record(amount=1568,
+            comment='Наполнение потребительской корзины',
+            date='09.03.2019')
+r3 = Record(amount=691, comment='Катание на такси', date='08.03.2019')
+
+# для CaloriesCalculator
+r4 = Record(amount=1186,
+            comment='Кусок тортика. И ещё один.',
+            date='24.02.2019')
+r5 = Record(amount=84, comment='Йогурт.', date='23.02.2019')
+r6 = Record(amount=1140, comment='Баночка чипсов.', date='24.02.2019') 
